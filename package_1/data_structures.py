1f63ce81ac6310b56b90500620ad7e3a7179ece9
sedaisagar <sagarsedai1997@gmail.com>
2026-05-14 20:55:44 +0545

    update::>> map filter sort

--- package_1/data_structures.py
+++ package_1/data_structures.py
@@ -81,13 +81,13 @@
 # List
 
 # students = []
-students = [
-    {'name': 'B', 'age': '13', 'address': 'asd', 'grade': 'asd', 'section': 'asd'},
-    {'name': 'D', 'age': '17', 'address': 'ads', 'grade': 's', 'section': 's'}, 
-    {'name': 'G', 'age': '16', 'address': 'sad', 'grade': 'sda', 'section': 'sd'}, 
-    {'name': 'J', 'age': '11', 'address': 'asd', 'grade': 'sd', 'section': 'sda'},
-    {'name': 'A', 'age': '12', 'address': 'asdfasdf', 'grade': 'asdf', 'section': 'asd'}, 
-]
+# students = [
+#     {'name': 'B', 'age': '13', 'address': 'asd', 'grade': 'asd', 'section': 'asd'},
+#     {'name': 'D', 'age': '17', 'address': 'ads', 'grade': 's', 'section': 's'}, 
+#     {'name': 'G', 'age': '16', 'address': 'sad', 'grade': 'sda', 'section': 'sd'}, 
+#     {'name': 'J', 'age': '11', 'address': 'asd', 'grade': 'sd', 'section': 'sda'},
+#     {'name': 'A', 'age': '12', 'address': 'asdfasdf', 'grade': 'asdf', 'section': 'asd'}, 
+# ]
 
 # for i in range(5):
     
@@ -110,13 +110,77 @@ students = [
 
 #     students.append(student)
 
-students.insert(100, {'name': 'E', 'age': '14', 'address': 'asdfasdf', 'grade': 'asdf', 'section': 'asd'}, )
+# students.insert(100, {'name': 'E', 'age': '14', 'address': 'asdfasdf', 'grade': 'asdf', 'section': 'asd'}, )
 
-print(students) 
-print("-"*50)
-students.sort(key=lambda x: x.get("age"), reverse=True)
-print(students) 
+# print(students) 
+# print("-"*50)
+# students.sort(key=lambda x: x.get("age"), reverse=True)
+# sorted_students = sorted(students, key=lambda x: x.get("age"), reverse=True)
+# print(students) 
+# print(sorted_students) 
+
+
+# MAP, SORTED, FILTER, COMPREHENSION
+
+# Map is a func, that transforms data in a list
+
+# students = [
+#     {'name': 'B', 'age': '13', 'address': 'asd', 'grade': 'asd', 'section': 'asd'},
+#     {'name': 'D', 'age': '17', 'address': 'ads', 'grade': 's', 'section': 's'}, 
+#     {'name': 'G', 'age': '16', 'address': 'sad', 'grade': 'sda', 'section': 'sd'}, 
+#     {'name': 'J', 'age': '11', 'address': 'asd', 'grade': 'sd', 'section': 'sda'},
+#     {'name': 'A', 'age': '12', 'address': 'asdfasdf', 'grade': 'asdf', 'section': 'asd'}, 
+# ]
+
+# transformed_students = list(map(lambda x : {"age":x.get("age")}, students))
+# print(transformed_students)
+
+# min_age = 15
+# filtered_students = list(filter(lambda x : int(x.get("age")) >= min_age, students))
+
+# print(filtered_students)
+
+
+# Write a python program to
+
+# Product Management
+# Scan for 5 items (products) from terminal
+# Product (name, category, price, stock_qty)
+    # name , category => str
+    # price => float, stock_qty => positive integer
+
+# 0. Filter products that are in stock (stock_qty > 0)
+# 1. Sort first on the basis of price descending
+# 2. Sort second on the basis of name ascending
+# 3. Map the whole dict in string like "Watch - Wearable - 500" 
 
 
 
 
+products = [
+    {
+        "name": input("Enter name of product \t"),
+        "category": input("Enter product category \t"),
+        "price": float(input("Enter product price \t")),
+        "stock_qty": int(input("Enter stock quantity \t")),
+    } for _ in range(5)
+]
+
+print("="*50)
+
+filtered_products = list(filter(lambda x : x["stock_qty"] > 0 , products))
+
+sorted_first  = sorted(products, key=lambda x: x["price"], reverse=True)
+
+sorted_second  = sorted(products, key=lambda x: x["name"], reverse=False)
+
+mapped_products = list(map(lambda x: f"{x["name"]} - {x["category"]} - {x["price"]}", products))
+
+print(filtered_products)
+print("="*50)
+print(sorted_first)
+print("="*50)
+print(sorted_second)
+print("="*50)
+print(mapped_products)
+print("="*50)
